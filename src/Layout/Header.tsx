35931e7148d624bb947a4d0f40bf0b3916e7a552
Haider Alshamma <haidera@nulogy.com>
2021-11-11 17:13:57 -0500

    Add background triangles

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { Box } from "../Box";
 import type { BoxProps } from "../Box/Box";
 import { Flex } from "../Flex";
@@ -10,6 +11,34 @@ type HeaderProps = BoxProps & {
   children?: React.ReactNode;
 };
 
+const RightAngleTriangle = styled(Box)`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  clip-path: polygon(100% 0, 0 100%, 100% 100%);
+`;
+
+const BackgroundTriangles = (props) => {
+  return (
+    <Box
+      position="absolute"
+      bottom={0}
+      right={0}
+      height="100%"
+      width="55%"
+      {...props}
+    >
+      <RightAngleTriangle
+        height="45%"
+        background="linear-gradient(173.35deg, #E0E3E6 5.22%, rgba(205, 219, 237, 0) 104.74%);"
+        opacity="0.5"
+      />
+      <RightAngleTriangle height="20%" bg="darkblue" opacity="0.05" />
+    </Box>
+  );
+};
+
 const Header: React.FC<HeaderProps> = ({
   background = "whiteGrey",
   breadcrumbs,
@@ -17,16 +46,19 @@ const Header: React.FC<HeaderProps> = ({
   children,
   ...rest
 }) => (
-  <Box p="x3" bg={background} {...rest}>
-    {breadcrumbs}
-    <Flex alignItems="center" mt="x1">
-      {title && <Heading1 mb="0">{title}</Heading1>}
-      {children && (
-        <Box ml="x1" flexGrow={1}>
-          {children}
-        </Box>
-      )}
-    </Flex>
+  <Box p="x3" position="relative" bg={background} {...rest}>
+    <Box zIndex={2} position="relative">
+      {breadcrumbs}
+      <Flex alignItems="center" mt="x1">
+        {title && <Heading1 mb="0">{title}</Heading1>}
+        {children && (
+          <Box ml="x1" flexGrow={1}>
+            {children}
+          </Box>
+        )}
+      </Flex>
+    </Box>
+    <BackgroundTriangles zIndex={1} />
   </Box>
 );
 
